e89731d076a77fefa8e270ee2fb344446570d5a7
Anton Georgiev <antongeorgiev313@gmail.com>
2019-04-05 14:19:36 +0300

    Create KNN_XOR.py

---
+++ Supervised Learning/KNN_XOR.py
@@ -0,0 +1,21 @@
+from __future__ import print_function, division
+from builtins import range, input
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+
+from knn import KNN
+from util import get_xor
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    X, Y = get_xor()
+
+    # display the data
+    plt.scatter(X[:,0], X[:,1], s=100, c=Y, alpha=0.5)
+    plt.show()
+
+    # get the accuracy
+    model = KNN(3)
+    model.fit(X, Y)
+    print("Accuracy:", model.score(X, Y))
