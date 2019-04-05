5ca8e688976ea8fb41b5bfc1989a40c28771327b
Anton Georgiev <antongeorgiev313@gmail.com>
2019-04-05 14:03:44 +0300

    comment correction

--- Supervised Learning/utils.py
+++ Supervised Learning/utils.py
@@ -12,7 +12,7 @@ def get_data(limit=None):
     df = pd.read_csv('../large_files/train.csv')
     data = df.values
     np.random.shuffle(data)
-    X = data[:, 1:] / 255.0 # data is from 0..255
+    X = data[:, 1:] / 255.0 # if data is from 0..255
     Y = data[:, 0]
     if limit is not None:
         X, Y = X[:limit], Y[:limit]
