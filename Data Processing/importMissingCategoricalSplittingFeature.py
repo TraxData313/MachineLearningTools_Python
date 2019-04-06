cad7d42433f845ec97872c8ce14f7ce6ad86128e
Anton Georgiev <antongeorgiev313@gmail.com>
2019-04-06 15:26:21 +0300

    Update and rename importMissingCategoricalSplittingFeature.txt to importMissingCategoricalSplittingFeature.py

--- Data Processing/importMissingCategoricalSplittingFeature.py
+++ Data Processing/importMissingCategoricalSplittingFeature.py
@@ -3,16 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing the dataset
 dataset = pd.read_csv('C:/Users/Trax/Desktop/00 Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
 
 # Encoding categorical data
 # Encoding the Independent Variable
@@ -25,9 +28,11 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
