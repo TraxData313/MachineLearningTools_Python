f1517598f54b28cbc69f76843ffb0fb269d6c144
Anton Georgiev <antongeorgiev313@gmail.com>
2019-04-10 17:57:17 +0300

    added a reminder

--- Linear_Regression/MultiVarLinRegress_sklearn/backwardElimVariables.py
+++ Linear_Regression/MultiVarLinRegress_sklearn/backwardElimVariables.py
@@ -41,6 +41,7 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 # Building the optimal model using Backward Elimination
+# Reminder: we are eliminating high P values (ex > 0.5). High P : low impact.
 import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
